9fb5f4a1e7bc9db1cb6c0aa92ee8e1917a364ad8
Alex Barnes <barnesalexander1706@gmail.com>
2024-03-01 16:45:59 +0000

    Created Sliding Door Prefab

---
+++ Assets/SlidingDoorManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlidingDoorManager : MonoBehaviour
+{
+    public SkinnedMeshRenderer SlidingDoor;
+    public Transform openPos;
+    public Transform closePos;
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(movedoor());
+    }
+
+
+
+    public IEnumerator movedoor()
+    {
+        SlidingDoor.SetBlendShapeWeight(0, SlidingDoor.GetBlendShapeWeight(0) + 2.5f);
+        if (SlidingDoor.GetBlendShapeWeight(0) >= 100)
+        {
+            SlidingDoor.SetBlendShapeWeight(0, 0);
+        }
+        yield return new WaitForSeconds(0.05f);
+        StartCoroutine(movedoor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+
+    }
+
+
+
+}
